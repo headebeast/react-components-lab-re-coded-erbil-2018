969af04fa872f57137a6ffc3eb4297411385fd84
Hedi Sabah <headesabah@gmail.com>
2018-09-12 15:50:27 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
@@ -6,22 +9,60 @@ const BUTCHER_PRODUCTS = [
 ]
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {
+export default class OlderCoaster extends Component {
   // your code here
+    render() {
+      return (
+      <div class="oldercoaster">
+   <p>Two grannies having the time of their life!</p>
+   <p>Passengers:</p>
+   <ul>
+     <li>Agnes</li>
+     <li>Muriel</li>
+   </ul>
+ </div>
+ )
+  }
 }
 
-export class InFrontOfYou extends React.Component {
+export default class InFrontOfYou extends Component {
   // your code here
+  render(){
+    return(
+      <div>
+  <p>You shouldn't look too far.</p>
+  <p>Sometimes, the solution is right in front of you.</p>
+</div>
+)
+  }
 }
-
-export class ButcherShop extends React.Component {
+export class ButcherShop extends Component {
   // your code here
+  render(){
+    return(
+      <div className="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    <li>Tenderloin</li>
+    <li>Short ribs</li>
+    <li>Beef shin</li>
+    <li>Ribeye</li>
+  </ul>
+</div>
+      )
+  }
 }
-
-
-ReactDOM.render(
-  <div>
-    render your components here! Refer back to the previous README if you are lost
-  </div>,
+export class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      <OlderCoaster />      
+      <InFrontOfYou />      
+      <ButcherShop />
+      </div>
+      )
+  }
+}
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
